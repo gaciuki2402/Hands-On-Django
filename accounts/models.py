66538e49c45e5abd198366d25ce4dcf7ffc609a2
gaciuki2402 <delbrivigrace@gmail.com>
2022-02-05 12:35:07 +0300

    create Leads model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Leads(models.Model):
+    username=models.CharField(max_length=156,unique=True)
+    email=models.EmailField(max_length=157,unique=True)
+    active=models.BooleanField(default=False)
+    full_name=models.CharField(max_length=256)
+    phone=models.CharField(max_length=15,unique=True)
+    town=models.CharField(max_length=57)
+
+
+    def __str__(self):
+        return f"{self.username}- {self.email}"
+
+    class Meta:
+        verbose_name_plural="Leads"
